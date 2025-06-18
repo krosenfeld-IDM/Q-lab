cdf0898e0d9ab06df6649e83e2af2311b29df7c3
Katherine <krosenf@gmail.com>
2025-06-18 15:19:45 +0000

    Add Python devcontainer configuration and new post-create script, replacing the slim version

--- .devcontainer/python/postCreateCommand.sh
+++ .devcontainer/python/postCreateCommand.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-apt update
-apt install curl
 
-curl -LsSf https://astral.sh/uv/install.sh | sh
 uv venv
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
