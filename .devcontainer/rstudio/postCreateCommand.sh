60ec14ab6da615e1661f5208e488d7a2354af7ef
Katherine <krosenf@gmail.com>
2025-06-18 16:16:10 +0000

    Enhance postCreateCommand.sh scripts for Python and RStudio to include error handling and install 'gh' and 'jq' packages.

--- .devcontainer/rstudio/postCreateCommand.sh
+++ .devcontainer/rstudio/postCreateCommand.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+set -e  # Exit on any error
+
+sudo apt update && sudo apt install -y gh jq
+
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
